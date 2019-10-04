5c1f8c06168144272b9cd056f7f0168848fa572e
Nabille Helal <nabilleh@outlook.com>
2019-10-04 15:19:02 +0100

    Add HP for player 1

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,7 +4,7 @@ feature '#attack' do
   scenario 'allows player_1 to attack player_2 and get a confirmation' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content "#{@player_1_name} attacked #{@player_2_name}"
+    expect(page).to have_content "#{@player_1_name} attacked the other chap"
   end
 
   scenario 'player_1 attacks player_2 and takes off 10hp' do
